5eaa755c7ddd2ba2fbeecae5f13e70915b0e1bf3
Akamitori <akamitori@tutanota.com>
2024-10-18 01:49:12 +0300

    Initial commit
    Initial commit
    
    Initial commit

---
+++ Camera.cpp
@@ -0,0 +1,47 @@
+﻿//
+// Created by PETROS on 15/10/2024.
+//
+
+#include "Camera.h"
+
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
+constexpr float camera_speed = 0.05f;
+
+void MoveCameraZ(Camera &camera, float modifier) {
+    camera.cameraPos += camera.camera_front * modifier * camera_speed;
+}
+
+void MoveCameraX(Camera &camera, float modifier) {
+    glm::vec3 camera_right = glm::normalize(glm::cross(camera.camera_front, camera.camera_up));
+    camera.cameraPos += camera_right * modifier * camera_speed;
+}
+
+glm::mat4 CameraLookAtMatrix(Camera &camera) {
+    return glm::lookAt(camera.cameraPos, camera.cameraPos + camera.camera_front, camera.camera_up);
+}
+
+glm::mat4 PerspectiveMatrix(float FOV, float z_near, float z_far, float width, float height) {
+    glm::mat4 perspectiveMatrix(0);
+
+    float tan_fov_2=glm::tan (glm::radians(FOV/2));
+    float tan_fov_2_invert=1/tan_fov_2;
+    float aspect_inverse=height/width;
+
+    float z_diff=1/(z_near-z_far);
+    perspectiveMatrix[0].x=tan_fov_2_invert*aspect_inverse;
+    perspectiveMatrix[1].y=tan_fov_2_invert;
+    perspectiveMatrix[2].z=(z_far+z_near)*z_diff;
+    perspectiveMatrix[2].w=-1;
+    perspectiveMatrix[3].z=-2*z_near*z_far*z_diff;
+    return perspectiveMatrix;
+}
+
+void PerspectiveMatrixUpdate(glm::mat4 &perspectiveMatrix, float FOV,float width, float height) {
+    float aspect_inverse=height/width;
+    float tan_fov_2_invert=1.f/glm::tan(glm::radians(FOV/2));
+    perspectiveMatrix[0].x=tan_fov_2_invert*aspect_inverse;
+    perspectiveMatrix[1].y=tan_fov_2_invert;
+}
+
---
+++ Camera.h
@@ -0,0 +1,30 @@
+﻿//
+// Created by PETROS on 15/10/2024.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <glm/fwd.hpp>
+#include <glm/geometric.hpp>
+#include <glm/vec3.hpp>
+
+struct Camera;
+
+void MoveCameraZ(Camera &camera, float modifier);
+void MoveCameraX(Camera &camera, float modifier);
+
+glm::mat4 CameraLookAtMatrix(Camera &camera);
+glm::mat4 PerspectiveMatrix(float FOV, float z_near, float z_far, float width, float height);
+void PerspectiveMatrixUpdate(glm::mat4 &perspectiveMatrix, float FOV,float width, float height);
+
+struct Camera {
+    glm::vec3 cameraPos{0.0f, 0.0f, 3.0f};
+    glm::vec3 camera_target{0.0f, 0.4f, 0.0f};
+    glm::vec3 camera_front{0,0,-1.f};
+    glm::vec3 camera_up{0.0f, 1.0f,  0.0f};
+    glm::vec3 camera_direction = glm::normalize(cameraPos - camera_target);
+    float cameraSpeed=0.5f;
+};
+
+
+#endif //CAMERA_H
