c9339a45ca6dabf7157d480bf616d30e625157d3
Akamitori <akamitori@tutanota.com>
2024-10-30 00:22:31 +0200

    Free look camera properly moves around and rotates in 3D space

---
+++ math_ops.h
@@ -0,0 +1,23 @@
+﻿//
+// Created by PETROS on 29/10/2024.
+//
+
+#ifndef MATH_OPS_H
+#define MATH_OPS_H
+//#include <glm/gtc/constants.hpp>
+
+
+namespace math_ops {
+    constexpr float PI = 3.14; //glm::pi<float>();
+    constexpr float TWOPI = 3.14; //glm::two_pi<float>();
+    constexpr float PI_angle = 180;
+
+    float normalize_angles(float angle);
+
+    float normalize_radians(float angle_in_radians);
+
+    bool is_equal(float a, float b, float EPSILON = 0.000001f);
+}
+
+
+#endif //MATH_OPS_H
