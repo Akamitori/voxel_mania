73a0fe0e9521990b81f75e7097ada6dc2341a51a
Akamitori <akamitori@tutanota.com>
2024-12-03 00:23:07 +0200

    Implemented Perlin Noise with Fractal Brownian motion

--- math_ops.h
+++ math_ops.h
@@ -1,22 +1,22 @@
-﻿//
-// Created by PETROS on 29/10/2024.
-//
-
-#ifndef MATH_OPS_H
+﻿#ifndef MATH_OPS_H
 #define MATH_OPS_H
-#include <glm/gtc/constants.hpp>
-
 
 namespace math_ops {
-    constexpr float PI = glm::pi<float>();
-    constexpr float TWOPI =  glm::two_pi<float>();
+    constexpr float PI = 3.14159265358979323846264338327950288;
+    constexpr float TWOPI = 6.28318530717958647692528676655900576;
     constexpr float PI_angle = 180;
 
     float normalize_angles(float angle);
 
     float normalize_radians(float angle_in_radians);
 
+
     bool is_equal(float a, float b, float EPSILON = 0.000001f);
+
+    bool is_equal(double a, double b, double EPSILON = 0.000001f);
+
+    float lerp(float value, float start, float end);
+    double lerp(double value, double start, double end);
 }
 
 
