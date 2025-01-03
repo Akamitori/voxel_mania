12dfcc7a9f283365d275408d3633d12e8a188a12
Akamitori <akamitori@tutanota.com>
2025-01-03 03:34:13 +0200

    Implemented our own Vector4d and matrix 4d

---
+++ libraries/math/Vector4D.cpp
@@ -0,0 +1,43 @@
+﻿#include "Vector4D.h"
+
+
+Vector4D &operator *=(Vector4D &v, float s) {
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+    v.w *= s;
+    return v;
+}
+
+Vector4D &operator /=(Vector4D &v, float s) {
+    s = 1.0F / s;
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+    v.w *= s;
+    return v;
+}
+
+Vector4D &operator +=(Vector4D &v1, const Vector4D &v2) {
+    v1.x += v2.x;
+    v1.y += v2.y;
+    v1.z += v2.z;
+    v1.w += v2.w;
+    return v1;
+}
+
+Vector4D &operator -=(Vector4D &v1, const Vector4D &v2) {
+    v1.x -= v2.x;
+    v1.y -= v2.y;
+    v1.z -= v2.z;
+    v1.w -= v2.w;
+    return v1;
+}
+
+Vector4D operator *(const Vector4D &v, float s) {
+    return (Vector4D(v.x * s, v.y * s, v.z * s, v.w * s));
+}
+
+Vector4D operator +(const Vector4D &a, const Vector4D &b) {
+    return (Vector4D(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w));
+}
---
+++ libraries/math/Vector4D.h
@@ -0,0 +1,24 @@
+﻿#ifndef VECTOR4D_H
+#define VECTOR4D_H
+
+#include "export.h"
+
+struct Vector4D
+{
+    float		x, y, z, w;
+};
+
+EXPORTED Vector4D& operator *=(Vector4D& v,float s);
+
+EXPORTED Vector4D& operator /=(Vector4D& v,float s);
+
+EXPORTED Vector4D& operator +=(Vector4D& v1, const Vector4D& v2);
+
+EXPORTED Vector4D& operator -=(Vector4D& v1,const Vector4D& v2);
+
+EXPORTED Vector4D operator *(const Vector4D &v, float s);
+
+EXPORTED Vector4D operator +(const Vector4D &a, const Vector4D &b);
+
+
+#endif //VECTOR4D_H
