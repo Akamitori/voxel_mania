ba62f87d71ed2299fa48967f6bc32779b2b26955
Akamitori <akamitori@tutanota.com>
2025-01-12 13:17:05 +0200

    Implemented a Plane struct

---
+++ libraries/math/Plane.cpp
@@ -0,0 +1,19 @@
+﻿#include "Plane.h"
+
+
+Plane::Plane(): normal{}, w{} {
+}
+
+Plane::Plane(float nx, float ny, float nz, float d) : normal{nx, ny, nz}, w{d} {
+}
+
+Plane::Plane(const Vector3D &n, float d) : normal{n}, w{d} {
+}
+
+float dot_vector(const Plane &plane, const Vector3D &v) {
+    return plane.normal.x * v.x + plane.normal.y * v.y + plane.normal.z * v.y;
+}
+
+float dot_point(const Plane &plane, const Vector3D &p) {
+    return plane.normal.x * p.x + plane.normal.y * p.y + plane.normal.z * p.y + plane.w;
+}
