42012415d6044ba6318ef7b6253300093fb71c03
Akamitori <akamitori@tutanota.com>
2025-03-31 23:18:03 +0300

    Removed constexpr

--- libraries/math/Trigonometry.h
+++ libraries/math/Trigonometry.h
@@ -9,14 +9,14 @@ constexpr float PI_angle = 180;
 constexpr float TO_DEGREES_MULTIPLIER = 57.295779513082320876798154814105;
 constexpr float TO_RADIANS_MULTIPLIER = 0.01745329251994329576923690768489;
 
-EXPORTED constexpr float DegreeToRadians(float degrees);
+EXPORTED float DegreeToRadians(float degrees);
 
-EXPORTED constexpr float RadiansToDegrees(float radians);
+EXPORTED float RadiansToDegrees(float radians);
 
-EXPORTED constexpr float normalize_radians(float radians);
+EXPORTED float normalize_radians(float radians);
 
-EXPORTED constexpr float normalize_angle(float angle);
+EXPORTED float normalize_angle(float angle);
 
-EXPORTED constexpr float fabs_const(float value); 
+EXPORTED float fabs_const(float value);
 
 #endif //TRIGONOMETRY_H
