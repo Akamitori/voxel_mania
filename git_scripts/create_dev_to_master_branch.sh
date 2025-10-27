dc5812bd350343f381e15f6e9b6c10e903071259
Akamitori <akamitori@tutanota.com>
2025-10-28 01:15:41 +0200

    Added custom script for merging branches to master

---
+++ git_scripts/create_dev_to_master_branch.sh
@@ -0,0 +1,12 @@
+﻿# 1️⃣ Checkout master and create a temporary branch
+git checkout master
+git pull origin master        # make sure it's up to date
+git checkout -b dev-on-master
+
+# 2️⃣ Cherry-pick all non-merge commits from dev onto the new branch
+git log --no-merges --reverse --format="%H" master..dev | while read commit; do
+    DATE="$(git show -s --format=%aI $commit)"
+    MSG="$(git log -1 --format=%B $commit)"
+    git cherry-pick --no-commit $commit
+    GIT_COMMITTER_DATE="$DATE" git commit --date "$DATE" -m "$MSG"
+done
